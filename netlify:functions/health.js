ea3ed754e896be033dc0ff7cfcb0709cda62b310
firsterr <ilkersahins@gmail.com>
2025-09-04 18:13:39 +0300

    Delete netlify:functions directory

--- netlify:functions/health.js
+++
@@ -1,7 +0,0 @@
-export async function handler() {
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ ok: true, service: 'health-stub' }),
-  };
-}
