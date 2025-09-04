f9fd3dcc3dad6f46aa11af01e8a288c83772b547
firsterr <ilkersahins@gmail.com>
2025-09-04 18:18:38 +0300

    Create forms-list.js

---
+++ mikroar-form-app/netlify/functions/forms-list.js
@@ -0,0 +1,4 @@
+export async function handler(event) {
+  if (event.httpMethod !== 'GET') return { statusCode: 405, body: 'Method Not Allowed' };
+  return { statusCode: 200, headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ ok: true, service: 'forms-list-stub' }) };
+}
