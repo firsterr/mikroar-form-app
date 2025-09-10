488ccd2499ec90ca947ddeb200800dce411a96a0
firsterr <ilkersahins@gmail.com>
2025-09-10 14:59:33 +0300

    Update forms.js

--- mikroar-form-app/netlify/functions/forms.js
+++ mikroar-form-app/netlify/functions/forms.js
@@ -39,8 +39,12 @@ export async function handler(event) {
 
 function json(code, obj) {
   return {
-    statusCode: code,
-    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
-    body: JSON.stringify(obj)
-  };
+  statusCode: 200,
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8',
+    // Netlify edge cache:
+    'Cache-Control': 'public, max-age=0, s-maxage=60, stale-while-revalidate=30'
+  },
+  body: JSON.stringify({ ok: true, schema })
+};
 }
